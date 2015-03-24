c25aed0cb1867ba8879c26935d590e3c7e5ed0cd
calve <calvinh34@gmail.com>
2015-03-24 18:21:18 +0100

    A nice SiteImpl

--- src/main/SiteImpl.java
+++ src/main/SiteImpl.java
@@ -7,10 +7,17 @@ import java.rmi.server.UnicastRemoteObject;
 public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 
 	protected RemoteRef ref;
-	protected SiteImpl pere;
+	protected int pere;
 	protected SiteImpl[] fils;
 
-	protected SiteImpl(RemoteRef ref, SiteImpl pere, SiteImpl[] fils) throws RemoteException {
+	protected SiteImpl(RemoteRef ref) throws RemoteException {
+		super();
+		this.ref  = ref;
+		this.pere = -1;
+		this.fils = null;
+	}
+
+	protected SiteImpl(RemoteRef ref, int pere, SiteImpl[] fils) throws RemoteException {
 		super();
 		this.ref  = ref;
 		this.pere = pere;
@@ -20,7 +27,7 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 	@Override
 	public int sendMessage(byte[] message, RemoteRef initiateur)
 			throws RemoteException {
-		
+
 		if (this.fils.equals(null)) {
 			System.out.println("DEBUG ["+this.ref+"] : Plus de fils");
 			return 0;
@@ -32,5 +39,31 @@ public class SiteImpl extends UnicastRemoteObject implements SiteItf {
 			}
 			return result;
 		}
+	}
+
+	/*  The first argument is the father id
+	    The other arguments are childrens ids
+	 */
+	public static void main(String[] args) {
+		boolean has_father = false;
+		int father;
+		if (args.length > 0){
+			has_father = true;
+			father = Integer.parseInt(args[0]);
+			System.out.println("father : "+father);
+		}
+		try {
+			SiteImpl siteImpl = (SiteImpl) new SiteImpl(null);
+			System.out.println("Running");
+		}
+		catch (Exception e) {
+			System.out.println("Something bad happend");
+			e.printStackTrace();
+		}
+
+		/* Lire l'id du père dans args */
+		/* Récupère une instance du père auprès de RMIregistry */
+		/* new SiteImpl(? id_pere) */
+		/* while (true) ?*/
 	}
 }
